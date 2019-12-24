084fe5e0a4bfba29cfb780daa92eb539f0018634
danbulant <danbulant@gmail.com>
2019-12-24 09:58:19 +0100

    First theme setup

---
+++ index.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron')
+
+var win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    if(process.env.NODE_ENV == "dev"){
+        win.loadFile('view/index.html')
+    }
+    win.removeMenu();
+    win.setMenu(null);
+    win.webContents.openDevTools()
+
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
