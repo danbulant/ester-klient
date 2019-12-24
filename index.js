bee04e1339f9c22f81faa86dc95b471b0b079265
danbulant <danbulant@gmail.com>
2019-12-24 11:50:11 +0100

    Working on chat & simple graphics

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, protocol, session, ipcMain, globalShortcut, BrowserWindow, Menu } = require('electron');
 
 var win;
 
@@ -6,17 +6,34 @@ function createWindow() {
     win = new BrowserWindow({
         width: 800,
         height: 600,
+        fullscreen: true,
+        autoHideMenuBar: true,
+        kiosk: true,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            nodeIntegrationInWorker: true
         }
     })
-
-    if(process.env.NODE_ENV == "dev"){
-        win.loadFile('view/index.html')
+    globalShortcut.register('f11', () => {
+        //win.setKiosk(!win.isKiosk());
+        win.setFullScreen(!win.isFullScreen());
+     });
+     
+     if (process.env.NODE_ENV == "dev") {
+        win.webContents.openDevTools()
+         
+        globalShortcut.register('Ctrl+Alt+Escape', () => {
+             app.quit();
+        });
+    } else {
+        globalShortcut.register('ctrl+r', () => { });
+        window.onbeforeunload = (e) => {
+            e.returnValue = false;
+        }
     }
+    win.loadFile('view/index.html')
     win.removeMenu();
     win.setMenu(null);
-    win.webContents.openDevTools()
 
     win.on('closed', () => {
         win = null
---
+++ view/user.js
@@ -0,0 +1,4 @@
+const username = "Daniel Bulant";
+
+var accountElement = document.getElementById("username");
+accountElement.innerText = username;
